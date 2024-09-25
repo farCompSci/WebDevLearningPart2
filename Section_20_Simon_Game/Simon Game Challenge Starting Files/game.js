5c1af9eff5bba48b1185eecf4a5b693904e9b7e1
Farjad Andrianina MADATALY <farjadandrianinamadataly@Farjads-MacBook-Pro.local>
2024-09-25 00:04:36 -0700

    started working on the simon game

---
+++ Section_20_Simon_Game/Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,46 @@
+
+/**
+ * Computes a random number bewteen 1 and 3
+ *
+ * @params - None
+ * @return {number} Randomly generated number between 1 and 3.
+ */
+function nextSequence(){
+    let randomNumber = Math.floor(Math.random() * 4); // Random number between 0 and 3
+    return randomNumber;
+}
+
+/**
+ * Plays one of the sounds depending on the name of the file
+ * 
+ * @param {string} name - name of the file, excluding the extenstion
+ * @returns {None} - plays a sound
+ */
+function playSound(name){
+    let audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+
+
+// Logging the random pattern 
+let gamePattern = [];
+let buttonColours = ["red", "blue", "green", "yellow"];
+let userClickedPattern = [];
+
+// Getting the random number and adding it to the game pattern
+let randomChosenColour = buttonColours[nextSequence()];
+gamePattern.push(randomChosenColour);
+
+// Making the selected button flash
+$(`#${randomChosenColour}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+// Logging Button Clicks and appending to list of buttons clicked by user
+ $('.btn').on('click',(event)=>{
+    let userChosenColour = event.target.id;
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+ })
+
+
+
