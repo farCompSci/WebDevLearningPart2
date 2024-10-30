9c02979ba6c329d7838d0f2bc89bb666d9aab070
Farjad Andrianina MADATALY <farjadandrianinamadataly@Farjads-MacBook-Pro-45.local>
2024-10-29 22:18:09 -0700

    Complete API Auth Coding

---
+++ Section_28_Using_API/5.4 API Authentication/obtaining_keys.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+// Registering new user
+// await axios.post("https://secrets-api.appbrewery.com/register", 
+//     {
+//         username: "redacted",
+//         password: "redacted"
+//     }
+// ).then((response) => {
+//     console.log(response.data);
+// }).catch((error) => {
+//     console.error("Error:", error.response ? error.response.data : error.message);
+// });
+
+
+// Getting an API Bear Token
+// axios.post("https://secrets-api.appbrewery.com/get-auth-token",
+//     {
+//         username: "redacted",
+//         password: "redacted"
+//     }
+//     ).then((response) => {
+//         console.log(response.data);
+//     })
+
+// Getting Normal API Token
+// axios.get("https://secrets-api.appbrewery.com/generate-api-key").then(response => {
+//     console.log(response.data);
+// })
+
+// Testing API Key
