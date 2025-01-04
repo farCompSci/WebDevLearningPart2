15c3df1c5aa6b0bdaf4e73e90187fe7f9425e8ed
Farjad Andrianina MADATALY <farjadandrianinamadataly@Mac.attlocal.net>
2025-01-03 17:43:55 -0800

    complete task 2

--- Section_33_PostgreSQL/8.3 Travel Tracker/index.js
+++ Section_33_PostgreSQL/8.3 Travel Tracker/index.js
@@ -30,7 +30,23 @@ app.get("/", async (req, res) => {
     countries.push(country.country_code);
   })
   res.render("index.ejs",{'countries':countries, total:countries.length});
-  db.end();
+  // db.end(); do not do this, it's a mistake
+});
+
+app.post("/add",async (req,res)=>{
+  const input = req.body["country"];
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1",
+    [input]
+  );
+  if (result.rows.length > 0){
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      countryCode,
+    ])
+  }
+  res.redirect("/");
 });
 
 app.listen(port, () => {
