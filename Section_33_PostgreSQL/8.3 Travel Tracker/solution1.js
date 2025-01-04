75f4a170cb1e4251be5cab24fdfb06cb851e3d75
Farjad Andrianina MADATALY <farjadandrianinamadataly@Mac.attlocal.net>
2025-01-03 16:35:16 -0800

    Complete Challenge 1

--- Section_33_PostgreSQL/8.3 Travel Tracker/solution1.js
+++ Section_33_PostgreSQL/8.3 Travel Tracker/solution1.js
@@ -1,11 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
-import dotenv from "dotenv";
 import pg from "pg";
+import dotenv from "dotenv";
 
 const app = express();
 const port = 3000;
 
+// database connection setup
 dotenv.config();
 let pwd = process.env.DB_PASSORD;
 
@@ -14,22 +15,22 @@ const db = new pg.Client({
   host: "localhost",
   database: "postgres",
   password: pwd,
-  port: 5433
-})
+  port: 5433,
+});
 db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
+// GET home page
 app.get("/", async (req, res) => {
-  //Write your code here.
   const result = await db.query("SELECT country_code FROM visited_countries");
   let countries = [];
-  result.rows.forEach((country)=>{
+  result.rows.forEach((country) => {
     countries.push(country.country_code);
-  })
-  res.render("index.ejs",{'countries':countries, total:countries.length});
+  });
+  console.log(result.rows);
+  res.render("index.ejs", { countries: countries, total: countries.length });
   db.end();
 });
 
