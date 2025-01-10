b306d1123dba4aa8fc615ab06ac29a0c54b83eb4
Farjad Andrianina MADATALY <farjadmadataly18@gmail.com>
2025-01-09 20:29:58 -0800

    add form to collect the books and authors

--- Section_33_PostgreSQL/Book Notes Project/index.js
+++ Section_33_PostgreSQL/Book Notes Project/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import pg from "pg";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+import axios from "axios";
+
+const testURL = "https://covers.openlibrary.org/b/olid/OL27371038M-L.jpg"; // 12 Rules for Life Cover Image
 
 dotenv.config();
 
@@ -23,6 +26,15 @@ db.connect();
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
+});
+
+app.post("/add", (req, res) => {
+
+    const title = req.body.title;
+    const author = req.body.author;
+    const coverImg = req.body.coverImg;
+
+    res.redirect("/");
 });
 
 app.post("/edit", (req, res) => {});
